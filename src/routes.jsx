3f2cdeffdb8f4e3678573dd56bbcdfdac78344b6
liubomyr <liubomyrpoland@gmail.com>
2025-10-27 16:35:21 +0100

    created basic template of project's routes

---
+++ src/routes.jsx
@@ -0,0 +1,60 @@
+import App from "./App";
+import { createBrowserRouter, redirect } from "react-router";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
+function runOnce(fn) {
+  let called = false;
+
+  return function (...args) {
+    if (called) return;
+    called = true;
+    return fn(...args);
+  };
+}
+const redirectOnceToAuth = runOnce(() => {
+    throw redirect('auth')
+});
+export let router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        loader: () => {
+            if (!(window.location.pathname === "/")) return null
+            const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+                const [key, value] = cookie.split("=");
+                acc[key] = value;
+                return acc;
+            }, {});
+            
+            const userToken = cookies["userToken"]; 
+            
+            if (!userToken) {
+                redirectOnceToAuth()
+            }
+            
+            return null;
+        },
+        children: [
+            {
+                path: 'auth',
+                Component: AuthPage,
+                // loader exectutes before the route is rendered!
+                // loader: ({ request, params }) =>
+                    //   fetch(`/api/show/${params.showId}.json`, {
+                //     signal: request.signal,
+                //   }),
+            },
+            {
+                path: 'home',
+                Component: HomePage,
+            }
+        ],
+    },
+]);
+
+
+export const ROUTES = {
+  AUTH_URL: "/auth",
+  HOME_URL: "/home",
+};
