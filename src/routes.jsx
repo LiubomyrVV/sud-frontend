eed771278bfaf34e0b2159a8ffd0972f821f7116
liubomyr <liubomyrpoland@gmail.com>
2025-10-30 13:22:35 +0100

    rewrited router logic

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,51 +2,36 @@ import App from './App';
 import { createBrowserRouter, redirect } from 'react-router';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
-import { executeOnce } from './utils/utils';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-
-const redirectOnceToAuth = executeOnce(() => {
-  throw redirect('auth');
-});
+import { getUserInfo } from './api/api';
 
 export let router = createBrowserRouter([
   {
     path: '/',
     Component: App,
-    loader: () => {
-      if (!(window.location.pathname === '/')) return null;
-      const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-        const [key, value] = cookie.split('=');
-        acc[key] = value;
-        return acc;
-      }, {});
-
-      const userToken = cookies['userToken'];
-
-      if (!userToken) {
-        redirectOnceToAuth();
-      }
-
-      return null;
-    },
     children: [
       {
         path: 'auth',
         Component: AuthPage,
+        loader: async () => {
+          const data = await getUserInfo();
+          if (data?.email) throw redirect('/home');
+          return null;
+        },
         children: [
           { path: 'login', Component: LoginForm },
           { path: 'register', Component: RegisterForm },
         ],
-        // loader exectutes before the route is rendered!
-        // loader: ({ request, params }) =>
-        //   fetch(`/api/show/${params.showId}.json`, {
-        //     signal: request.signal,
-        //   }),
       },
       {
         path: 'home',
         Component: HomePage,
+        loader: async () => {
+          const data = await getUserInfo();
+          if (!data?.email) throw redirect('/auth');
+          return null;
+        },
       },
     ],
   },
